3a4985af0c55be08c8f1eb77331153ff566d4c16
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-03-12 23:05:15 +0100

    Override RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -61,5 +61,15 @@ namespace GradeBook.GradeBooks
 
             base.CalculateStatistics();
         }
+        
+        
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
