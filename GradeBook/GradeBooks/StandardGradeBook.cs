08a2a917f2f9ec42c9f3d4269fb8d55ee6b31ea2
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-03-12 21:48:08 +0100

    Add class StandardGradeBook and RankedGradeBook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
